434a06d9b1d8a36548fd1458657765c55ba575ae
naveen-nexus <nk4451@srmist.edu.in>
2026-03-23 14:48:13 +0530

    UC2:Basic Room Types & Static Availability

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,8 +1,66 @@
 public class BookMyStayApp {
-        public static void main(String[] args) {
-            System.out.println("Welcome to Book My Stay App!");
-            System.out.println("Hotel Booking Management System v1.0");
-            System.out.println("Application started successfully.");
+    abstract class Room {
+        String type;
+        int beds;
+        double price;
+
+        Room(String type, int beds, double price) {
+            this.type = type;
+            this.beds = beds;
+            this.price = price;
+        }
+
+        void display() {
+            System.out.println("Room Type: " + type);
+            System.out.println("Beds: " + beds);
+            System.out.println("Price: " + price);
         }
     }
+
+    class SingleRoom extends Room {
+        SingleRoom() {
+            super("Single Room", 1, 1000);
+        }
+    }
+
+    class DoubleRoom extends Room {
+        DoubleRoom() {
+            super("Double Room", 2, 1800);
+        }
+    }
+
+    class SuiteRoom extends Room {
+        SuiteRoom() {
+            super("Suite Room", 3, 3000);
+        }
+    }
+
+    public class UseCase2RoomInitialization {
+
+        public static void main(String[] args) {
+
+            Room single = new SingleRoom();
+            Room doub = new DoubleRoom();
+            Room suite = new SuiteRoom();
+
+            int singleAvailable = 5;
+            int doubleAvailable = 3;
+            int suiteAvailable = 2;
+
+            System.out.println("Book My Stay App - v2.1");
+
+            System.out.println("\n--- Room Details ---");
+
+            single.display();
+            System.out.println("Available: " + singleAvailable + "\n");
+
+            doub.display();
+            System.out.println("Available: " + doubleAvailable + "\n");
+
+            suite.display();
+            System.out.println("Available: " + suiteAvailable + "\n");
+        }
+    }
+
+}
 
