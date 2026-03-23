7e3c0ec33d019de8789ac9edac3b7a1569f536e0
naveen-nexus <nk4451@srmist.edu.in>
2026-03-23 14:41:44 +0530

    UC1: Add welcome message module

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,2 +1,8 @@
 public class BookMyStayApp {
-}
+        public static void main(String[] args) {
+            System.out.println("Welcome to Book My Stay App!");
+            System.out.println("Hotel Booking Management System v1.0");
+            System.out.println("Application started successfully.");
+        }
+    }
+
